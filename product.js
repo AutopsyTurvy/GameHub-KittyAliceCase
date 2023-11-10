ebfb552c176fa4594c3e5736425f455a31240cfe
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2023-11-10 13:58:21 +0100

    I have been so confused and swamped i forgot to commit to main! Ops!
    I want more time on JS

--- product.js
+++ product.js
@@ -1,39 +1,56 @@
 const searchParams = new URLSearchParams(window.location.search);
-
-var baseUrl = "";
-
-if(searchParams.has('id')) {
-    baseUrl = `https://api.noroff.dev/api/v1/gamehub/${searchParams.get('id')}`;  
-
-}
-
-
-
+const baseUrl = "https://api.noroff.dev/api/v1/gamehub";
 
 async function fetchData() {
   try {
-    const response = await fetch(`${baseUrl}`);
- 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
- 
-    const data = await response.json();
-    console.log(data);
+      const response = await fetch(`${baseUrl}/${searchParams.get('id')}`);
 
-    var element = document.getElementById("product");
-   
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      // Check for specific conditions and handle errors
+      if (!data || typeof data !== 'object') {
+          throw new Error('Invalid data received from the server.');
+      }
+
+      var element = document.getElementById("product");
+
       element.innerHTML = `
-        <article>
-          <img src="${data.image}">
-          <h2>${data.title}</h2>
-          <p>${data.description}</p>
-        </article>
+          <article id="api-game-container">
+              <img src="${data.image}">
+              <h2>${data.title}</h2>
+              <p>${data.description}</p>
+              <p>Genre: ${data.genre}</p>
+              <p>Date of Release: ${data.released}</p>
+              <button id="buyButton">Buy Now</button>
+          </article>
       `;
-  
-  
+
+      var buyButton = document.getElementById("buyButton");
+      buyButton.addEventListener("click", function() {
+          console.log("Buy button clicked!");
+      });
 
   } catch (error) {
-    console.error(error);
+
+      if (error instanceof TypeError) {
+          console.error('There was a network error or the server did not respond.');
+      } else {
+          console.error(error.message);
+      }
   }
 }
+
+fetchData();
+
+
+
+
+
+
+
+
+
--- products.js
+++ products.js
@@ -1,34 +1,50 @@
 const baseUrl = "https://api.noroff.dev/api/v1/gamehub";
 
+
+
 async function fetchData() {
   try {
     const response = await fetch(`${baseUrl}`);
- 
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
- 
-    const data = await response.json();
-    console.log(data);
 
-    
-    var element = document.getElementById("products");
-    for(var i = 0; i < data.length; i++) {
-      element.innerHTML += `
+
+
+const data = await response.json();
+console.log(data);
+
+
+
+var element = document.getElementById("products");
+for (var i = 0; i < data.length; i++) {
+    const buttonId = `buyButton${i}`;
+
+    element.innerHTML += `
         <article id="api-game-container">
-          <a href="product.html?id=${data[i].id}">
-            <img id="api-image" src="${data[i].image}">
-            <h2>${data[i].title}</h2>
-            <p>${data[i].description}</p>
-          </a>
+            <a href="product.html?id=${data[i].id}">
+                <img id="api-image" src="${data[i].image}">
+                <h2>${data[i].title}</h2>
+                <p>${data[i].description}</p>
+                <div class="extra-info">
+                    <p>Genre: ${data[i].genre}</p>
+                    <p>Date of Release: ${data[i].released}</p>
+                    <button id="${buttonId}" class="buy-button">Buy Now</button>
+                </div>
+            </a>
         </article>
-      `;
-    }
-  
+    `;
 
-    } catch (error) {
-        console.error(error);
-    }
+    var buyButton = document.getElementById(buttonId);
+    buyButton.addEventListener("click", function() {
+        window.location.href = `product.html?id=${data[i].id}`;
+    });
+}
+
+
+} catch (error) {
+    console.error(error);
+}
 }
 
 
