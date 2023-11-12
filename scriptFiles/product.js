9b56c0efe2443d7a31d2bc515674bc973186e960
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2023-11-12 22:00:52 +0100

    i want to throw my computer through the window

--- scriptFiles/product.js
+++ scriptFiles/product.js
@@ -3,81 +3,69 @@
 const searchParams = new URLSearchParams(window.location.search);
 const baseUrl = "https://api.noroff.dev/api/v1/gamehub";
 
+function fetchData() {
+  document.getElementById("loading").style.display = "block";
 
-async function fetchData() {
-  try {
-
-
-      const response = await fetch(`${baseUrl}/${searchParams.get('id')}`);
-      if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      if (!data || typeof data !== 'object') {
-          throw new Error('Invalid data received from the server.');
-      }
-
+  const response = fetch(`${baseUrl}/${searchParams.get('id')}`)
+    .then(response => response.json())
+    .then(data => {
       var element = document.getElementById("product");
 
       const isOnSale = data.onSale;
-    const displayedPrice = isOnSale ? data.discountedPrice : data.price;
+      const displayedPrice = isOnSale 
+        ? data.discountedPrice 
+        : data.price;
+  
+      const priceText = isOnSale
+        ? `This item is on sale! Discounted price is: $${data.discountedPrice}`
+        : `Price: ${data.price}`;
+  
+      const exchangeRate = 11.11;
+      const priceInKroner = displayedPrice * exchangeRate;
+  
+      element.innerHTML = `
+        <img src="${data.image}">
+        <h2>${data.title}</h2>
+        <p>${data.description}</p>
 
+        <div class="genreandagerating">
+            <p>Genre: ${data.genre}</p>
+            <p>Age Rating: ${data.ageRating}</p>
+        </div>
 
-    const priceText = isOnSale
-  ? `This item is on sale! Discounted price is: $${data.discountedPrice}`
-  : `Price: ${data.price}`;
+        <p>${priceText} / ${priceInKroner.toFixed(2)} Kr</p>
 
+        <p>Date of Release: ${data.released}</p>
+        <a href="#" id="addToCartButton-${data.id}" class="add-to-cart-link">Add to Cart!</a>
+      `;
 
+      document.getElementById(`addToCartButton-${data.id}`)
+        .addEventListener("click", (e) => {
+          addProductToCart(data.id);
+          addNumberOfItemsToCartIcon();
+          e.preventDefault();
+          return false;
+        });
+    })
+    .catch(error => {
+      console.error("An error occurred:", error.message);
 
+      var element = document.getElementById("product");
+      const errorId = "singleProductError";
+      element.innerHTML = `<p id="${errorId}" class='singleProductError'>Oops! An error occurred while fetching the API!</p>`;
+    });
 
-  const exchangeRate = 11.11;
-  const priceInKroner = displayedPrice * exchangeRate;
+  document.getElementById("loading").style.display = "none";
+}
 
-
-element.innerHTML = `
-  <article id="api-game-container">
-      <img src="${data.image}">
-      <h2>${data.title}</h2>
-      <p>${data.description}</p>
-
-  <div class="genreandagerating">
-      <p>Genre: ${data.genre}</p>
-      <p>Age Rating: ${data.ageRating}</p>
-  </div>
-
-      <p>${priceText} / ${priceInKroner.toFixed(2)} Kr</p>
-
-      <p>Date of Release: ${data.released}</p>
-      <a href="./Pages/yourcart.html" id="buyButton" class="add-to-cart-link">Add to Cart!</a>
-  </article>
-`;
-
-
-
- var buyButton = document.getElementById("buyButton");
-    buyButton.addEventListener("click", function() {
-});
-
-
-
-} catch (error) {
-    console.error("An error occurred:", error.message);
-
-    var element = document.getElementById("product");
-    const errorId = "singleProductError";
-    element.innerHTML = `<p id="${errorId}" class='singleProductError'>Oops! An error occurred while fetching the API!</p>`;
+function addProductToCart(id) {
+  if(localStorage.getItem("cart") === null) {
+    localStorage.setItem("cart", id);
+  } else {
+      var cart = localStorage.getItem("cart").split(",");
+      cart.push(id);
+      localStorage.setItem("cart", cart.join(","));
   }
 }
 
 fetchData();
-
-
-
-
-
-
-
-
-
--- scriptFiles/products.js
+++ scriptFiles/products.js
@@ -4,6 +4,8 @@ const baseUrl = "https://api.noroff.dev/api/v1/gamehub";
 const element = document.getElementById("products");
 
 async function fetchData() {
+  document.getElementById("loading").style.display = "block";
+
   try {
     const response = await fetch(baseUrl);
     const data = await response.json();
@@ -11,24 +13,19 @@ async function fetchData() {
     if (data.length === 0) {
       element.innerHTML = "<p>No data available</p>";
     } else {
-      element.innerHTML = data.map((item, i) => `
+      element.innerHTML = `
         <article id="api-game-container">
-          <a href="product.html?id=${item.id}">
+          <a href="Pages/product.html?id=${item.id}">
             <img id="api-image" src="${item.image}">
             <h2>${item.title}</h2>
             <div class="extra-info">
               <p>Genre: ${item.genre}</p>
-              <button id="buyButton${i}" class="buy-button">Click for more Info</button>
+              <button id="buyButton-${item.id}" class="buy-button">Click for more Info</button>
             </div>
           </a>
         </article>
-      `).join('');
-
-      data.forEach((item, i) => addEventListenerToBuyButton(`buyButton${i}`, item.id));
+      `;
     }
-
-
-
   } catch (error) {
     console.error("An error occurred:", error.stack);
     const errorMessageContainer = document.createElement("div");
@@ -39,24 +36,8 @@ async function fetchData() {
     errorMessageContainer.className = "error-container";
     element.appendChild(errorMessageContainer);
   }
+
+  document.getElementById("loading").style.display = "none";
 }
-
-
-
-
-function addEventListenerToBuyButton(buttonId, productId) {
-
-  document.getElementById(buttonId).addEventListener("click", async () => {
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    window.location.href = `product.html?id=${productId}`;
-});
-}
-
-
-
-function showLoading() {
-  document.getElementById("loading").style.display = "block";
-}
-showLoading();
 
 fetchData();
