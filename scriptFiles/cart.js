407b9f2f34d8cba13cf2d945b4210519002f8791
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2023-12-08 21:24:49 +0100

    Suggested solution--

--- scriptFiles/cart.js
+++ scriptFiles/cart.js
@@ -1,8 +1,44 @@
-//cart.js
+// cart.js
 
-function addNumberOfItemsToCartIcon() {
-    var cartButton = document.getElementById("cartbutton");
-    cartButton.innerHTML = `cart (${localStorage.getItem("cart").split(",").length})`
+function displayCartItems() {
+    var cartItems = localStorage.getItem("cart");
+    var emptyCartText = document.querySelector(".empty-cart-text");
+    var cartContainer = document.getElementById("cart-container");
+
+    if (cartItems) {
+        cartItems = cartItems.split(",");
+
+        if (cartItems.length > 0) {
+            const products = cartItems.map(itemId => {
+                const productDetails = localStorage.getItem(`product_${itemId}`);
+                return productDetails ? JSON.parse(productDetails) : null;
+            }).filter(product => product !== null);
+
+            if (products.length > 0) {
+                cartContainer.innerHTML = products.map(product => {
+                    return `
+                        <div class="cart-item">
+                            <img src="${product.image}" alt="${product.title}">
+                            <p>Title: ${product.title}</p>
+                            <p>Price: ${product.price}</p>
+                        </div>
+                    `;
+                }).join('');
+
+                emptyCartText.textContent = "";
+            } else {
+                cartContainer.innerHTML = ""; 
+                emptyCartText.textContent = "Your cart is empty.";
+            }
+        } else {
+            cartContainer.innerHTML = ""; 
+            emptyCartText.textContent = "Your cart is empty.";
+        }
+    } else {
+        cartContainer.innerHTML = ""; 
+        emptyCartText.textContent = "Your cart is empty.";
+    }
 }
-addNumberOfItemsToCartIcon();
 
+
+displayCartItems();
