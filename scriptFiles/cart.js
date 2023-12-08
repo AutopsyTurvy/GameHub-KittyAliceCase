c6f97cb2c223648ce723a271d8c60f049a083e8c
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2023-12-08 16:32:26 +0100

    fixing :)

--- scriptFiles/cart.js
+++ scriptFiles/cart.js
@@ -12,9 +12,24 @@ document.addEventListener("DOMContentLoaded", function () {
       cartItems = cartItems.split(",");
   
       if (cartItems.length > 0) {
-        cartContainer.innerHTML = cartItems.map(itemId => {
-          return `<p>Item ID: ${itemId}</p>`;
-        }).join('');
+        const products = cartItems.map(itemId => {
+          const productDetails = localStorage.getItem(`product_${itemId}`);
+          return productDetails ? JSON.parse(productDetails) : null;
+        }).filter(product => product !== null);
+  
+        if (products.length > 0) {
+          cartContainer.innerHTML = products.map(product => {
+            return `
+              <div class="cart-item">
+                <img src="${product.image}" alt="${product.title}">
+                <p>Title: ${product.title}</p>
+                <p>Price: ${product.price}</p>
+              </div>
+            `;
+          }).join('');
+        } else {
+          cartContainer.innerHTML = "<p>Your cart is empty.</p>";
+        }
       } else {
         cartContainer.innerHTML = "<p>Your cart is empty.</p>";
       }
