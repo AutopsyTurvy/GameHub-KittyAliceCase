7e72ac60a57addf8e90a550b514f55c648c98b2b
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2023-12-09 18:31:40 +0100

    error handling

--- scriptFiles/product.js
+++ scriptFiles/product.js
@@ -1,27 +1,28 @@
 // Product.js - The Product details
 
-
 const searchParams = new URLSearchParams(window.location.search);
 const productBaseUrl = "https://api.noroff.dev/api/v1/gamehub";
 
 function fetchProductData() {
+  var element; 
+
   const response = fetch(`${productBaseUrl}/${searchParams.get('id')}`)
     .then(response => response.json())
     .then(data => {
-      var element = document.getElementById("product");
+      element = document.getElementById("product"); 
 
       const isOnSale = data.onSale;
       const displayedPrice = isOnSale 
         ? data.discountedPrice 
         : data.price;
-  
+
       const priceText = isOnSale
         ? `This item is on sale! Discounted price is: $${data.discountedPrice}`
         : `Price: ${data.price}`;
-  
+
       const exchangeRate = 11.11;
       const priceInKroner = displayedPrice * exchangeRate;
-  
+
       element.innerHTML = `
         <img src="${data.image}">
         <h2>${data.title}</h2>
@@ -45,8 +46,7 @@ function fetchProductData() {
           e.preventDefault();
           return false;
         });
-      
-     
+
       function addProductToCart(id) {
         if(localStorage.getItem("cart") === null) {
           localStorage.setItem("cart", id);
@@ -60,7 +60,7 @@ function fetchProductData() {
     .catch(error => {
       console.error("An error occurred:", error.message);
 
-      var element = document.getElementById("product");
+      element = document.getElementById("product"); 
       const errorId = "singleProductError";
       element.innerHTML = `<p id="${errorId}" class='singleProductError'>Oops! An error occurred while fetching the API!</p>`;
     })
