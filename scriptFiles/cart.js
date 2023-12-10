01e4a4b6c7b767351bce62855b4390726f01b239
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2023-12-10 13:43:47 +0100

    images added due to feedback

--- scriptFiles/cart.js
+++ scriptFiles/cart.js
@@ -1,8 +1,20 @@
 //cart.js
 
-function addNumberOfItemsToCartIcon() {
-    var cartButton = document.getElementById("cartbutton");
-    cartButton.innerHTML = `cart (${localStorage.getItem("cart").split(",").length})`
-}
-addNumberOfItemsToCartIcon();
+function renderCartItems() {
+    const cartContainer = document.getElementById('cart-container');
+    const cartItems = localStorage.getItem('cart') ? localStorage.getItem('cart').split(',') : [];
 
+    if (cartItems.length === 0) {
+        cartContainer.innerHTML = '<p>Your cart is empty.</p>';
+    } else {
+        cartItems.forEach(id => {
+            const itemElement = document.createElement('p');
+            itemElement.textContent = `Product ID: ${id}`;
+            cartContainer.appendChild(itemElement);
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    renderCartItems();
+});
